a27551e06da0544abed40b9e41a8733cb8ba9e9b
heksen84 <heksen84@yandex.ru>
2022-11-29 14:35:05 +0600

    *

--- src/router.php
+++ src/router.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ .'/models/users.php';
-require_once __DIR__ .'/models/companies.php';
+require_once __DIR__ .'/entity/users.php';
+require_once __DIR__ .'/entity/companies.php';
 
 class Router {
  
